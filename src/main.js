b8989f42884a4d01fd0f5d4e4c5936bd80869db6
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2021-04-30 19:44:38 +0200

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import Vue from 'vue'
+import App from './App.vue'
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
