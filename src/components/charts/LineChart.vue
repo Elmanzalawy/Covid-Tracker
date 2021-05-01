52e9b44cf98b12d0a3873a12399f71e19e4f7e2c
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2021-05-01 03:16:19 +0200

    Added vue-chartjs package

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,22 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+    name:"LineChart",
+    extends: Line,
+    props: {
+        chartData: {
+            type: Object,
+            default: null
+        },
+        options: {
+            type: Object,
+            default: null
+        }
+    },
+    mounted () {
+        console.log(this.chartData)
+        this.renderChart(this.chartData, this.options)
+    }
+}
+</script>
